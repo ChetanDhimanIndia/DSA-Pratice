aacdfc7536bb3bcafdd572ea9d9fe61ba77d6c2e
chetandhimanindia <chetan.arethos@gmail.com>
2022-03-26 23:00:07 +0530

    First

---
+++ Array/TripletSumArray.cpp
@@ -0,0 +1,20 @@
+// Date : 21/03/2022
+
+/*
+Question:
+Given an array and a value, find if there is a triplet in array whose sum is equal to the given value.
+If there is such a triplet present in array, then print the triplet and return true. Else return false.
+*/
+
+/*
+Example :
+Input: array = {12, 3, 4, 1, 6, 9}, sum = 24;
+Output: 12, 3, 9
+Explanation: There is a triplet (12, 3 and 9) present
+in the array whose sum is 24.
+*/
+
+/*
+Approch Follow :
+
+*/
